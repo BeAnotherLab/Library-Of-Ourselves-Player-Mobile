488024d073ff421d6506c7f03de9c5fd7a2ad2eb
Jonathan Kings <jkings@hotmail.fr>
2018-08-11 16:17:04 +0200

    Fixed visual problems in guide ui

--- BeAnother/Assets/Scripts/Guide/Instruction1FromLanguage.cs
+++ BeAnother/Assets/Scripts/Guide/Instruction1FromLanguage.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class Instruction1FromLanguage : MonoBehaviour {
 	
 	void Start(){
-		GetComponent<Text>().text = Lang.GetText("instruction1") + CurrentSelection.Objects;
+		GetComponent<Text>().text = Lang.GetText("instructions") + "\n\n" + Lang.GetText("instruction1") + CurrentSelection.Objects;
+		for(int i = 2; i<=6; ++i){
+			GetComponent<Text>().text += "\n" + Lang.GetText("instruction"+i);
+		}
 	}
 	
 }
