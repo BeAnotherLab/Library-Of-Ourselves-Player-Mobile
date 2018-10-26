b9d5bf834a3485cf5755560b9d1d939ed99e7da1
Jonathan Kings <jkings@hotmail.fr>
2018-10-26 12:43:56 +0100

    Fixed connection and sync bugs - guide video is fullscreen now

---
+++ BeAnother/Assets/Scripts/VideoPlayback/StableFramedVideo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class StableFramedVideo : MonoBehaviour {
+	
+	void Awake(){
+		VideoPlayer player = GetComponent<VideoPlayer>();
+		if(!player.skipOnDrop){
+			print("Player will now skip on drop.");
+			player.skipOnDrop = true;
+		}
+		if(player.timeReference != VideoTimeReference.InternalTime){
+			print("Player will use internal clock as reference.");
+			player.timeReference = VideoTimeReference.InternalTime;
+		}
+	}
+	
+}
