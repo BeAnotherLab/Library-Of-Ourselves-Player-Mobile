28d4fe3c905e605afc8ec90c95d28e363cccbe92
Jonathan Kings <jkings@hotmail.fr>
2019-07-30 21:30:51 +0100

    App should now work properly even after idling for 30+ mins

---
+++ LibraryOfOurselves/Assets/Scripts/LongRunning.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongRunning : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_ANDROID
+		UnityEngine.Android.AndroidDevice.SetSustainedPerformanceMode(true);
+#endif
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+}
