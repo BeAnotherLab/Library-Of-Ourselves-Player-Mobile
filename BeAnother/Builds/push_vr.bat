c484a030e4149ead3877452dc9a5c58113936c61
Jonathan Kings <jkings@hotmail.fr>
2019-08-31 15:38:44 +0200

    [Up to date user apks] - changed it from single VR apk to two - one for Oculus and one for Daydream - hopefully this fixes any compatibility issues with Oculus devices:)

--- BeAnother/Builds/push_vr.bat
+++
@@ -1,6 +0,0 @@
-c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d uninstall sco.forgotten.beanother
-c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d install -r vr.apk
-c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d shell pm grant sco.forgotten.beanother android.permission.READ_EXTERNAL_STORAGE
-c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d shell pm grant sco.forgotten.beanother android.permission.WRITE_EXTERNAL_STORAGE
-c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d shell dumpsys package sco.forgotten.beanother
-pause
