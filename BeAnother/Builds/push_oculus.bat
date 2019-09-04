33454737c3a136ac71c06cc7f3d44bacc7b2edde
Jonathan Kings <jkings@hotmail.fr>
2019-09-04 11:26:23 +0100

    [Updated Oculus apk & batch files] - Fixed Oculus sdk version

--- BeAnother/Builds/push_oculus.bat
+++ BeAnother/Builds/push_oculus.bat
@@ -1,8 +1,8 @@
 echo "Make sure the file 'oculus.apk' is available in the current folder, then press enter to upload to the OculusGo or Samsung S8 currently plugged into the computer."
 pause
-c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d uninstall sco.Haze.LibraryOfOurselves
+c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d uninstall sco.forgotten.beanother
 c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d install -r oculus.apk
-c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d shell pm grant sco.Haze.LibraryOfOurselves android.permission.READ_EXTERNAL_STORAGE
-c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d shell pm grant sco.Haze.LibraryOfOurselves android.permission.WRITE_EXTERNAL_STORAGE
-c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d shell dumpsys package sco.Haze.LibraryOfOurselves
+c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d shell pm grant sco.forgotten.beanother android.permission.READ_EXTERNAL_STORAGE
+c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d shell pm grant sco.forgotten.beanother android.permission.WRITE_EXTERNAL_STORAGE
+c:/users/%username%/appdata/local/android/sdk/platform-tools/adb -d shell dumpsys package sco.forgotten.beanother
 pause
