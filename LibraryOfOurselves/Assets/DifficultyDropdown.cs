ed6f1cbc1eb968c088121d992f408167e683160d
ReallyRad <arthur.pointeau@gmail.com>
2024-03-11 16:25:14 +0100

    translate dropdown difficulty options too

--- LibraryOfOurselves/Assets/DifficultyDropdown.cs
+++ LibraryOfOurselves/Assets/DifficultyDropdown.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Haze;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,7 +12,9 @@ public class DifficultyDropdown : MonoBehaviour
     [SerializeField] private Image _arrow;
     [SerializeField] private Outline _outline;
     [SerializeField] private Dropdown _dropdown;
-
+    [SerializeField] private GameObject[] _translatedGameObjectsNames; //Easy, Normal, Hard
+    [SerializeField] private Text _difficultyText;
+    
     public Difficulty selectedDifficulty;
     
     public void EditMode(bool edit)
@@ -19,10 +22,15 @@ public class DifficultyDropdown : MonoBehaviour
         _arrow.enabled = edit;
         _outline.enabled = edit;
         _dropdown.enabled = edit;
+        if (edit)
+        {
+            for (int i = 0; i < 3; i++) _dropdown.options[i].text = _translatedGameObjectsNames[i].name;
+        }
     }
 
     public void DropdownOptionSelected(int option)
     {
+        _difficultyText.text = _translatedGameObjectsNames[option].name;
         if (option == 0) selectedDifficulty = Difficulty.Easy;
         else if (option == 1) selectedDifficulty = Difficulty.Normal;
         else if (option == 2) selectedDifficulty = Difficulty.Hard;
