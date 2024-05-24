825b99fc8fc83bea8350d8ef070d87dda74fb27d
ReallyRad <arthur.pointeau@gmail.com>
2024-05-24 19:36:11 +0200

    fixed auth behavior

--- LibraryOfOurselves/Assets/Scripts/Guide/Menu/SettingsAuth.cs
+++ LibraryOfOurselves/Assets/Scripts/Guide/Menu/SettingsAuth.cs
@@ -6,18 +6,26 @@ using UnityEngine.UI;
 
 public class SettingsAuth : MonoBehaviour
 {
-	public static bool unlocked = false; 
+	public static bool unlocked; 
 
 	[SerializeField] private InputField _pinInput;
 	[SerializeField] private UnityEvent _openSettingsPanel;
-	
+	[SerializeField] private GameObject _authFrame;
 	public static void SetPin(string pin)
 	{
 		HazePrefs.SetString("settings-pin", pin);
 	}
 	
 	public void OpenAuthPanel() {
-		if (unlocked) _openSettingsPanel.Invoke(); //we're already good
+		if (unlocked) //if we're already unlocked
+		{
+			_openSettingsPanel.Invoke(); //we're already good
+		}
+		else
+		{
+			_authFrame.SetActive(true);
+			_pinInput.text = "";
+		}
 	}
 
 	public void CloseAuthPanel() {
@@ -33,6 +41,7 @@ public class SettingsAuth : MonoBehaviour
 		if (currentPin == _pinInput.text) { 
 			unlocked = true;
 			CloseAuthPanel();
+			_openSettingsPanel.Invoke();
 			//TODO why do we need to update that display? 
 			if (VideoDisplay.expandedDisplay != null) VideoDisplay.expandedDisplay.Expand();//update that display.
 		}
